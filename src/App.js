e20cdb5e46a8a6191bc2d6ac064c81500f5fbe92
Anuj <anuj47kumar@gmal.com>
2021-03-08 18:27:54 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, {useState, useEffect} from 'react';
+import "./index.css";
+  
+  export default function App() {
+  const [users, setUsers] =useState([]);
+  const getuser = async () => {
+    const res = await fetch("https://reqres.in/api/users/");
+    const json = await res.json();
+    setUsers(json.data);
+  };
+  useEffect(() => {
+  getuser();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1><i>User Profile in card</i></h1>
+      <div className="flex">
+        {
+          users.map((user) => {
+            return (
+              <div key={user.id} className="bgcolor">
+            
+              <img  src={user.avatar} />
+              
+                <h1>
+                  <b>{user.first_name}</b>
+                </h1>
+                <p>{user.email}</p>
+              
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
 
-export default App;
